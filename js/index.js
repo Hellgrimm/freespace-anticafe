726e74fe762fb135e6f1d4116f79cacd803391d7
Hellgrim <91741315+Hellgrimm@users.noreply.github.com>
2024-05-15 13:00:04 +0000

    0.1.10 - Shoping Cart section to Home page
    -Added Cart block
    -Added local delete func for deleting books from list
    -Edited code of the page
    -Deleted Hero section
    
    For now the books in cart are static, but in plans use DB to link books to different people.
    
    Next objective - Lib section for listing books.

--- js/index.js
+++ js/index.js
@@ -8,3 +8,29 @@ function ValidMail() {
     document.getElementById("footeremail").value = "";
     return valid;
 }
+
+// Reference to the item count and the overlay
+var itemCount = document.getElementById('item-count');
+var bookOverlay = document.getElementById('book-overlay');
+var cartContainer = document.getElementById('cart-container');
+
+// Mouseover event on item count to show the overlay
+itemCount.addEventListener('mouseover', function() {
+    bookOverlay.style.display = 'block';
+});
+
+// Mouseout event on cart container to hide the overlay
+// Checks if the mouse is actually outside the cart-container before hiding
+cartContainer.addEventListener('mouseleave', function(event) {
+    if (!cartContainer.contains(event.relatedTarget)) {
+        bookOverlay.style.display = 'none';
+    }
+});
+
+
+
+document.querySelectorAll('.close-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        this.parentElement.style.display = 'none'; // Hide the parent .book-item
+    });
+});
